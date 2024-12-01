7f77fba6d46c91b11509b428042464dea6c25d83
Raunak Mishra <89805496+raunakjr@users.noreply.github.com>
2024-12-02 02:41:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
-import x from "./data.js";
-import { firstname, lastname } from "./data.js";
+import x from "../data.js";
+import { firstname, lastname } from "../data.js";
 console.log(firstname.name);
 console.log(lastname.name);
 console.log(x.name);
-import ReactDOM from "react-dom";
+
+import { createRoot } from "react-dom/client";
+
 const h1 = <h1>Hello World</h1>;
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(h1);
+const root = createRoot(document.querySelector("#root"));
+
+// Create new card
+
+// const card = (
+//   <div className="card">
+//     <img
+//       src="https://i.guim.co.uk/img/media/18badfc0b64b09f917fd14bbe47d73fd92feeb27/189_335_5080_3048/master/5080.jpg?width=645&dpr=2&s=none&crop=none"
+//       alt=""
+//     />
+//     <div className="card-content">
+//       <h3>Iphone X</h3>
+//       <p>Apple</p>
+//       <p>
+//         <b>$10000</b>
+//       </p>
+//     </div>
+//   </div>
+// );
+
+// Rendering Multiple Cards but all have same content
+// const container = [card, card, card, card, card, card, card];
+// root.render(<div class="card-container">{container}</div>);
+// to manupulate the content of each card
+
+function Card(key, title, url, brand, price) {
+  return (
+    <div className="card" key={key}>
+      <img src={url} alt="" />
+      <div className="card-content">
+        <h3>{title}</h3>
+        <p>{brand}</p>
+        <p>
+          <b>{"$" + price}</b>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    const container2 = data.products.map((e) =>
+      Card(e.id, e.title, e.images[0], e.brand, e.price)
+    );
+    console.log(data);
+    root.render(<div className="card-container">{container2}</div>);
+  });
+
+// here order need to be maintained
+// React component is nothing but the react element having type function
